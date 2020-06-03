7de5ce8769224fa8ad54703ae84851f69e713c32
Kirill Ivanov <puaktopblm@yandex.ru>
2020-06-03 17:46:46 +0700

    Приложение запустилось с подключенными библиотеками омрона. Собирается под NetFramework_4.8

--- Form1.cs
+++ Form1.cs
@@ -8,13 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using OMRON.Compolet.CIPCompolet64;
+
 namespace common_compolet_pure
 {
     public partial class Form1 : Form
     {
+        private OMRON.Compolet.CIPCompolet64.CommonCompolet commonCompolet1;
         public Form1()
         {
             InitializeComponent();
+            commonCompolet1 = new OMRON.Compolet.CIPCompolet64.CommonCompolet(this.components);
         }
 
     }
